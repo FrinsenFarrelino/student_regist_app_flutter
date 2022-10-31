c3739ea3e8e7e715f5a18c14a96db4e60074eb83
Agustinus Frinsen Farrelino Yoses <frinsenfarrelino@gmail.com>
2022-10-31 13:51:25 +0700

    fix bug assertion failed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:student_regist_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
